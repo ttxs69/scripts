121e19baf189b85a412165f1d1e3cfd104cee0e1
ttxs69 <tuty277@126.com>
2020-01-15 16:22:11 +0800

    init

---
+++ delete_github_forked_repos.py
@@ -0,0 +1,12 @@
+from github import Github
+
+# using username and password
+g = Github("username", "password")
+
+for repo in g.get_user().get_repos():
+    # check forked
+    if repo.fork:
+        repo.delete()
+    else:
+        continue
+print("done")
